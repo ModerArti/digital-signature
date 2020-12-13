9033ca4dd8278afa8c8bdcc0384e2d9a5b5e1262
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-12-13 18:27:22 +0300

    Add some comments

--- src/main/scala/org/example/DigitalSignatureGenerator.scala
+++ src/main/scala/org/example/DigitalSignatureGenerator.scala
@@ -4,8 +4,10 @@ import scala.annotation.tailrec
 
 object DigitalSignatureGenerator {
 
+  // переменная, хранящая данные об эллиптической кривой
   val ellipticCurve = new EllipticCurve(751, -1, 1)
 
+  // основная функция программы для подсчёта k*G, r, z, s
   def main(sysArgs: Array[String]): Unit = {
     val h = 10
     val d = 5
@@ -24,29 +26,34 @@ object DigitalSignatureGenerator {
     val s = module(z * (h + d * r), generatingPoint.q)
     println(s"s = $s")
 
-    println(new Point(generatingPoint.q, r, s))
+    println(s"(r, s) = ${new Point(generatingPoint.q, r, s)}")
   }
 
+  // рекурсивная функция, считающая модуль числа
   @tailrec
   def module(number: Int, mod: Int): Int = {
     if (number > 0) number % mod
     else module(number + mod, mod)
   }
 
+  // рекурсивная функция, которая ищет число, обратное данному (по модулю)
   @tailrec
   def reverseNumber(number: Int, mod: Int, k: Int = 1): Int = {
     if (module(number * k, mod) == 1) k
     else reverseNumber(number, mod, k + 1)
   }
 
+  // рекурсивная функция, считающая произведение k * G
   @tailrec
   def calculatekG(k: Int, G: Point, acc: Point = new Point(13, 0, 0)): Point = {
     if (k == 0) acc
     else calculatekG(k - 1, G, G + acc)
   }
 
+  // класс, абстрагирующий в себе точку (её координаты и порядок)
   class Point(val q: Int, val x: Int, val y: Int) {
 
+    // оператор суммирования двух точек
     def +(other: Point): Point = {
       if (other == new Point(13, 0, 0)) this
       else if (this == other) {
@@ -66,6 +73,7 @@ object DigitalSignatureGenerator {
       }
     }
 
+    // метод для подсчёта лямбды при сложении двух одинаковых точек
     def calculateLambda(x1: Int, y1: Int, a: Int): Int = {
       val factor = if (y1 < 0) -1 else 1
 
@@ -75,6 +83,7 @@ object DigitalSignatureGenerator {
       module(numerator * reverseNumber(denominator, ellipticCurve.p), ellipticCurve.p)
     }
 
+    // метод для подсчёта лямбды при сложении двух разных точек
     def calculateLambda(x1: Int, y1: Int, x2: Int, y2: Int): Int = {
       val factor = if (x2 - x1 < 0) -1 else 1
 
@@ -84,10 +93,12 @@ object DigitalSignatureGenerator {
       module(numerator * reverseNumber(denominator, ellipticCurve.p), ellipticCurve.p)
     }
 
+    // метод для преобразования точки в строку
     override def toString = s"Point($x, $y)"
 
     def canEqual(other: Any): Boolean = other.isInstanceOf[Point]
 
+    // метод сравнения двух точек
     override def equals(other: Any): Boolean = other match {
       case that: Point =>
         (that canEqual this) &&
@@ -102,6 +113,7 @@ object DigitalSignatureGenerator {
     }
   }
 
+  // класс, хранящий данные об эллиптической кривой
   class EllipticCurve(val p: Int, val a: Int, val b: Int)
 
 }
