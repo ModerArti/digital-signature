ca3093f7e961b28056a4862b175ab1a30681023e
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-12-13 19:36:36 +0300

    Add checking of signature

--- src/main/scala/ru/tversu/DigitalSignatureChecker.scala
+++ src/main/scala/ru/tversu/DigitalSignatureChecker.scala
@@ -15,7 +15,30 @@ object DigitalSignatureChecker {
     val h = 5
     val Q = new Point(13, 596, 433)
     val generatingPoint = new Point(13, 562, 89)
-    
+
+    if (1 <= rs.x && rs.x <= generatingPoint.q - 1 &&
+      1 <= rs.y && rs.y <= generatingPoint.q - 1) {
+      val v = module(reverseNumber(rs.y, generatingPoint.q), generatingPoint.q)
+      println(s"v = $v")
+
+      val u1 = module(v * h, generatingPoint.q)
+      println(s"u1 = $u1")
+
+      val u2 = module(v * rs.x, generatingPoint.q)
+      println(s"u2 = $u2")
+
+      val X1 = calculatekG(u1, generatingPoint)
+      println(s"X1 = $X1")
+      val X2 = calculatekG(u2, Q)
+      println(s"X2 = $X2")
+      val X = X1 + X2
+      println(s"X = $X")
+
+      if (rs.x == module(X.x, generatingPoint.q)) println("Signature is original")
+      else println("Signature is fake")
+    } else {
+      println("Signature is fake")
+    }
   }
 
   // рекурсивная функция, считающая модуль числа
