c0e9d1fcf0889704d6ec3cfda0ebe4533c75b3ba
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-12-13 14:58:01 +0300

    Add tail recursion to calculatekG func

--- src/main/scala/org/example/DigitalSignatureGenerator.scala
+++ src/main/scala/org/example/DigitalSignatureGenerator.scala
@@ -28,15 +28,17 @@ object DigitalSignatureGenerator {
     else reverseNumber(number, k + 1)
   }
 
-  def calculatekG(k: Int, G: Point): Point = {
-    if (k == 1) G
-    else G + calculatekG(k - 1, G)
+  @tailrec
+  def calculatekG(k: Int, G: Point, acc: Point = new Point(13, 0, 0)): Point = {
+    if (k == 0) acc
+    else calculatekG(k - 1, G, G + acc)
   }
 
   class Point(val q: Int, val x: Int, val y: Int) {
 
     def +(other: Point): Point = {
-      if (this == other) {
+      if (other == new Point(13, 0, 0)) this
+      else if (this == other) {
         val lambda = module(calculateLambda(this.x, this.y, ellipticCurve.a))
 
         val newX = module(math.pow(lambda, 2).toInt - 2 * this.x)
@@ -72,6 +74,21 @@ object DigitalSignatureGenerator {
     }
 
     override def toString = s"Point($x, $y)"
+
+    def canEqual(other: Any): Boolean = other.isInstanceOf[Point]
+
+    override def equals(other: Any): Boolean = other match {
+      case that: Point =>
+        (that canEqual this) &&
+          x == that.x &&
+          y == that.y
+      case _ => false
+    }
+
+    override def hashCode(): Int = {
+      val state = Seq(x, y)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
   }
 
   class EllipticCurve(val p: Int, val a: Int, val b: Int)
